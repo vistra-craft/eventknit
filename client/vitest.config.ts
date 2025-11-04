49f3bc0514893c0d8703a5d4c71eeeb741ae6291
BrianKvin <briankvin@gmail.com>
2025-11-04 08:24:12 +0300

    added missing packages

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
