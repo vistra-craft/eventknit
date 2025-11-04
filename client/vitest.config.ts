e089374c21715e103559dfe3a485347946d39c03
BrianKvin <briankvin@gmail.com>
2025-11-04 08:20:01 +0300

    fixed more lint errors

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   test: {
