7d1eb2814c990b4303367119db6db5c2a9a8e86e
BrianKvin <briankvin@gmail.com>
2026-02-28 11:03:41 +0300

    resolved import error

--- server/src/services/admin-financial.service.ts
+++ server/src/services/admin-financial.service.ts
@@ -2,7 +2,6 @@ import { prisma } from '../config/database.js';
 import { NotFoundError, ValidationError } from '../utils/errors.js';
 import { logger } from '../utils/logger.js';
 import { Decimal } from '@prisma/client/runtime/library';
-import { Prisma } from '@prisma/client';
 
 export interface CreatePlatformExpenseData {
   category: string;
